13a57e43abfb3a74243edf64d30a53e4080dc343
FranciscoPlaza24 <franciscoprogramacion2024@gmail.com>
2024-05-21 19:42:11 -0300

    electronica

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
 
   return (
   
   <div className="App">
-      <header className="Header">
-        
+      <header className="Header"> 
       </header>
-      {/* Resto de tu contenido */}
+      <main>
+        <section className='seccion1'>  <div className="carousel">
+            <div className="carousel-track">
+              <img src="src/assets/imagenes/mainseccion1.jpg" alt="Imagen 1" />
+              <img src="src/assets/imagenes/mainseccion 1.jpg" alt="Imagen 2" />
+              <img src="src/assets/imagenes/mainseccion3.jpg" alt="Imagen 3" />
+            </div>
+          </div>
+          </section>
+        
+      </main>
+       
     </div>
+     
   )
 }
 
