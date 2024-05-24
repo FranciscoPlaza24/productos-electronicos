d660b2302774aecd96bd22b87cbbd7840adba5e4
FranciscoPlaza24 <franciscoprogramacion2024@gmail.com>
2024-05-24 01:16:07 -0300

    productos electronicos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import './index.css';
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +17,7 @@ function App() {
           <button className="menu-item">Home</button>
 
           <div className="menu-item dropdown">
-            Red Dragon
+            ReDragon
             
           </div>
 
@@ -44,8 +46,11 @@ function App() {
             </div>
           </div>
         </section>
+        <section id="footer"></section>
       </main>
     </div>
+
+    
   );
 }
 
